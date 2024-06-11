7527822c21f349c7c315de01902d52c065eb9bd1
pzagardedukic <petra.zagar.dedukic@gmail.com>
2024-06-11 14:22:22 +0200

    Tokenization Form Fix

--- src/components-ui-basic/Form/FormDropzoneBoxOneFile.tsx
+++ src/components-ui-basic/Form/FormDropzoneBoxOneFile.tsx
@@ -1,5 +1,5 @@
 import { useDropzone } from 'react-dropzone';
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 
 const iconImagePlus = `${process.env.PUBLIC_URL}/icons/icon-image-plus.svg`;
 
@@ -16,22 +16,64 @@ const FormDropzoneBoxOneFile: React.FC<FormDropzoneBoxOneFileProps> = ({ files, 
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()} style={{ width: '265px', border: '1px dashed #ccc', padding: '20px', textAlign: 'center', cursor: 'pointer' }}>
+    <Box
+      {...getRootProps()}
+      sx={{
+        width: '265px',
+        border: '2px dashed #D1D5DB',
+        borderRadius: '6px',
+        padding: '20px',
+        textAlign: 'center',
+        cursor: 'pointer'
+      }}
+    >
       <input {...getInputProps()} />
       {files.length > 0 ? (
         files.map((file, index) => (
-          <Typography key={index} variant="body2">
+          <Typography
+            key={index}
+            variant="body2"
+            style={{
+              fontFamily: 'Inter',
+              fontSize: '14px',
+              fontWeight: 500,
+              lineHeight: '20px',
+              textAlign: 'left'
+            }}
+          >
             {file.name}
           </Typography>
         ))
       ) : (
         <>
-          <img src={iconImagePlus} alt="icon" style={{ width: '50px', marginBottom: '10px' }} />
-          <Typography variant="body2">Upload a file</Typography>
-          <Typography variant="body2">or drag and drop here</Typography>
+          <Box component="img" src={iconImagePlus} alt="icon" sx={{ width: '50px' }} />
+          <Typography
+            variant="body2"
+            style={{
+              fontSize: '14px',
+              fontWeight: 500,
+              lineHeight: '1.7em',
+              textAlign: 'center',
+              color: '#4F46E5'
+            }}
+          >
+            Upload a file
+          </Typography>
+          <Typography
+            variant="body2"
+            style={{
+              fontSize: '12px',
+              fontWeight: 500,
+              lineHeight: '1.7em',
+              textAlign: 'center',
+              color: '#4B5563'
+            }}
+          >
+            or drag and drop here
+          </Typography>
         </>
       )}
-    </div>
+    </Box>
   );
 };
 
